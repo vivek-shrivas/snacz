8ff82f491693218402af9506a23d3f5926b498d8
vivek-shrivas <vivekshrivas347@gmail.com>
2024-01-13 00:09:59 +0530

    added layouts for user login and signup.

---
+++ app/src/main/java/com/example/snacz/activity_user_info.java
@@ -0,0 +1,14 @@
+package com.example.snacz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_user_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
