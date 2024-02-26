a86147bb41154103ba19e2fbf7849cd10f27a7fe
vivek-shrivas <vivekshrivas347@gmail.com>
2024-02-26 05:50:03 +0530

    finally completed

---
+++ app/src/main/java/com/example/snacz/Account.java
@@ -0,0 +1,2 @@
+package com.example.snacz;public class Account {
+}
---
+++ app/src/main/java/com/example/snacz/AccountAdapter.java
@@ -0,0 +1,24 @@
+package com.example.snacz;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public CartAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
